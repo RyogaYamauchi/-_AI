acdc3337d347c4616e5fd1188c01327ef9bbcf53
Ryoga Yamauchi <g1854003@gmail.com>
2020-06-24 11:01:55 +0900

    card battle

--- cardbattle/alpha/Z/Play.java
+++ cardbattle/alpha/Z/Play.java
@@ -1,7 +1,15 @@
 package Z;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Play implements P.Player {
 
+	private ArrayList<Integer> remainingYourCards = new ArrayList<Integer>();
+	private ArrayList<Integer> remainingMyCards = new ArrayList<Integer>();
+	private int[] beforeYourCards = new int[13];
 	public int call (
 		int[] mycard, int[] yourcard, 
 		int[] mylog, int[] yourlog, 
@@ -10,14 +18,57 @@ public class Play implements P.Player {
 		int count, long timeleft
 	)
 	{
-		int submit = 0;
-		java.util.Random r = new java.util.Random();
-		while (true) {
-			submit = r.nextInt(13) + 1;
-			if (mycard[submit]==0) {
-				return submit;
+		if(count == 1)
+		{
+			for(var i = 1; i < 14;i++)
+			{
+				remainingYourCards.add(i);
+				remainingMyCards.add(i);
+			}
+			return 10;
+		}
+
+		remainingYourCards.remove(remainingYourCards.indexOf(yourlog[count-1]));
+		remainingMyCards.remove(remainingMyCards.indexOf(mylog[count-1]));
+
+
+		int index = 0;
+		var random = new Random();
+		// 相手が２のカードを持っている場合、３〜９のカードを使う
+		if(remainingYourCards.contains(2))
+		{
+			if(hasFToS(remainingMyCards, 2, 10))
+			{
+				index = random.nextInt((int)remainingMyCards.stream().filter(x -> x > 2 && x < 10).count());
+				var a= remainingMyCards.stream().filter(x -> x > 2 && x < 10).collect(Collectors.toList()).get(index);
+				return a;
 			}
 		}
+		// 相手が2のカードを持っていない場合、絵札を使う
+		else {
+			if(hasFToS(remainingMyCards, 10, 14))
+			{
+				index = random.nextInt((int) remainingMyCards.stream().filter(x -> x > 10 && x < 14).count());
+				var a = remainingMyCards.stream().filter(x -> x > 10 && x < 14).collect(Collectors.toList()).get(index);
+				return a;
+			}
+		}
+
+		// 相手が２のカードを持っておらず、自身が絵札を使い切った場合、そのたを使う
+		index = random.nextInt(remainingMyCards.size());
+		return remainingMyCards.get(index);
 	}
 
+	private void p(String str)
+	{
+		System.out.println(str);
+	}
+	private void p(int i)
+	{
+		System.out.println(i);
+	}
+	private boolean hasFToS(ArrayList<Integer> list, Integer f, Integer s)
+	{
+		return list.stream().anyMatch(x -> x > f && x < s);
+	}
 }
